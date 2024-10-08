49269731d68ee7145966647ad0caefd66e5cd1e4
ajochymczyk <ajochymczykagh@gmail.com>
2024-10-08 10:58:27 +0200

    Initial commit

---
+++ backend/app/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, jsonify
+from .sentence_generator import generate_sentence
+
+api = Blueprint("api", __name__)
+
+@api.route("/generate_sentence", method=["POST"])
+def generate_sentence_endpoint():
+    data = request.get_json()
+
+    subject = data.get("subject")
+    verb = data.get("verb")
+    object_ = data.get("object")
+
+    if not (subject or verb or object_):
+        return  jsonify({"error": "Brak danych!"}), 400
+
+    sentence = generate_sentence(subject, verb, object_)
+
+    return jsonify({"sentence": sentence})
---
+++ backend/app/sentence_generator.py
@@ -0,0 +1,4 @@
+def generate_sentence(subject: str, verb: str, object_: str) -> str:
+    # docelowo gramatyka
+    sentence = f"{subject} {verb} {object_}."
+    return sentence
